fae11115471a8fa33b472eaeea4d7fcc12fd9d31
mariaciornii <70654766+mariaciornii@users.noreply.github.com>
2021-10-18 10:04:13 +0300

    Add files via upload

---
+++ problema 8.py
@@ -0,0 +1,23 @@
+list="A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
+a1 =""
+a2 = ""
+a3 = ""
+for i in list:
+     x=chr(ord(i)+1)
+     a1+=x
+     b=a1.replace ('!',' ')
+     s=b.replace ('[', 'A')
+print (" sirus a:", s)
+
+a2=v
+for i in list:
+      w=a2.replace (("Z"), ("A"))
+      v=w.replace ('!',' ')
+      k=v.replace ('[', 'A')
+print (" sirul b:" , k)
+ 
+ a3=w
+ for i in list:
+     p=a3. replace ('.','-')
+     b=p.replace  ('[', 'A')
+     print (" sirul c :" , b )
